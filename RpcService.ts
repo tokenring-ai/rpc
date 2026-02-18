debddb53cd98242238b2f8284ffad394c2c75918
Mark Dierolf <mdierolf@cloudquote.io>
2026-02-17 21:07:04 -0800

    Refactor `RpcService` to use `KeyedRegistry` for improved endpoint management

--- RpcService.ts
+++ RpcService.ts
@@ -1,4 +1,5 @@
 import {TokenRingService} from "@tokenring-ai/app/types";
+import KeyedRegistry from "@tokenring-ai/utility/registry/KeyedRegistry";
 import TypedRegistry from "@tokenring-ai/utility/registry/TypedRegistry";
 import {RpcEndpoint} from "./types.ts";
 
@@ -6,9 +7,12 @@ export default class RpcService implements TokenRingService {
   readonly name = "RpcService";
   description = "RPC endpoint registry and execution service";
 
-  private endpoints = new TypedRegistry<RpcEndpoint>();
+  private endpoints = new KeyedRegistry<RpcEndpoint>();
 
-  registerEndpoint = this.endpoints.register;
   getEndpoint = this.endpoints.getItemByName;
-  getAllEndpoints = this.endpoints.getItems
+  getAllEndpoints = this.endpoints.getAllItemValues
+
+  registerEndpoint(endpoint: RpcEndpoint) {
+    this.endpoints.register(endpoint.name, endpoint);
+  }
 }
