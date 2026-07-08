0b7342586ca077403f32f4700570cb9f4f5659bb
Mark Dierolf <mdierolf@financialcontent.com>
2026-07-07 17:52:27 -0700

    Moved `createAgentStateStream` from RPC to agent package

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-export type { AgentStateStreamYield, CreateAgentStateSliceStreamOptions, StateSubscribable } from "./createAgentStateStream.ts";
-export { createAgentStateSliceStream } from "./createAgentStateStream.ts";
 export type { CreatePollingQueryStreamOptions } from "./createPollingQueryStream.ts";
 export { createPollingQueryStream } from "./createPollingQueryStream.ts";
 export { default as RpcService } from "./RpcService.ts";
